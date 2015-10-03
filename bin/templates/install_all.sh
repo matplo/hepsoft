b097b170437dfc0fc10f43db49a10e6f1bd62d9c
matplo <ploskon@gmail.com>
2015-10-04 00:11:44 +0200

    setenv_all.sh in bin

--- bin/templates/install_all.sh
+++ bin/templates/install_all.sh
@@ -6,5 +6,3 @@ $XDIR/bin/install_hepmc.sh 		2>&1 | tee $XDIR/logs/install_hepmc.log
 $XDIR/bin/install_lhapdf.sh 	2>&1 | tee $XDIR/logs/install_lhapdf.log
 $XDIR/bin/install_fastjet.sh	2>&1 | tee $XDIR/logs/install_fastjet.log
 $XDIR/bin/install_pythia8.sh	2>&1 | tee $XDIR/logs/install_pythia8.log
-
-
---
+++ bin/templates/setenv_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+XDIR="<dir to be set>"
+
+. $XDIR/scripts/setenv_fastjet_3.1.2.sh
+. $XDIR/scripts/setenv_hepmc_2.06.09.sh
+. $XDIR/scripts/setenv_lhapdf_5.9.1.sh
+. $XDIR/scripts/setenv_pythia_8205.sh
+
