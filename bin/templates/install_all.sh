cd1b9178c535de897281c59a362182e3b802ac37
matplo <ploskon@gmail.com>
2015-10-04 00:05:43 +0200

    install all

---
+++ bin/templates/install_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+XDIR="<dir to be set>"
+mkdir -p $XDIR/logs
+$XDIR/bin/install_hepmc.sh 		2>&1 | tee $XDIR/logs/install_hepmc.log
+$XDIR/bin/install_lhapdf.sh 	2>&1 | tee $XDIR/logs/install_lhapdf.log
+$XDIR/bin/install_fastjet.sh	2>&1 | tee $XDIR/logs/install_fastjet.log
+$XDIR/bin/install_pythia8.sh	2>&1 | tee $XDIR/logs/install_pythia8.log
+
+
