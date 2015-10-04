4bfe0218b7473496a5f94f870d0cce4159761ee3
matplo <ploskon@gmail.com>
2015-10-04 14:19:33 +0200

    making sure things are set before installing pythia

--- bin/templates/install_all.sh
+++ bin/templates/install_all.sh
@@ -5,4 +5,9 @@ mkdir -p $XDIR/logs
 $XDIR/bin/install_hepmc.sh 		2>&1 | tee $XDIR/logs/install_hepmc.log
 $XDIR/bin/install_lhapdf.sh 	2>&1 | tee $XDIR/logs/install_lhapdf.log
 $XDIR/bin/install_fastjet.sh	2>&1 | tee $XDIR/logs/install_fastjet.log
+
+. $XDIR/scripts/setenv_fastjet_3.1.2.sh
+. $XDIR/scripts/setenv_hepmc_2.06.09.sh
+. $XDIR/scripts/setenv_lhapdf_5.9.1.sh
+
 $XDIR/bin/install_pythia8.sh	2>&1 | tee $XDIR/logs/install_pythia8.log
