6e137e945e99d72ed7ee3cddf24c0eca5a14c24d
matplo <ploskon@gmail.com>
2017-07-07 22:26:16 -0700

    accept $1 as directory where to addrpath

--- bin/addrpath.sh
+++ bin/addrpath.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-plib=`root-config --libdir`
+plib=$1
+[ -z $plib ] && plib=`root-config --libdir`
 
 if [ -d $plib ]; then
-	for i in $plib/lib*.so ; do
-		echo $i
-		install_name_tool -add_rpath $plib $i
-	done	
+	for i in $plib/lib*.so $plib/lib*.dylib ; do
+		if [ -e $i ]; then
+			echo $i
+			install_name_tool -add_rpath $plib $i
+		fi
+	done
 fi
