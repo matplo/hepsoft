0afc9430df999fc4d57ac24401d67309ad790652
matplo <ploskon@gmail.com>
2017-08-10 18:44:20 -0700

    still fixing

--- cmake/_install.sh
+++ cmake/_install.sh
@@ -6,54 +6,66 @@ wdir=${hepsoft_dir}
 savedir=$PWD
 cd $wdir
 source ${hepsoft_dir}/bin/tools.sh
-pdsf_modules="gcc python cmake/3.9.0"
+pdsf_modules="gcc python qt"
 module_name=$(module_name $BASH_SOURCE)
-clean=$(is_opt_set --clean)
+module_dir=${hepsoft_dir}/${module_name}
+do_clean=$(is_opt_set --clean)
 do_build=$(is_opt_set --build)
 do_make_module=$(is_opt_set --module)
 do_download=$(is_opt_set --download)
 
-[ $(is_opt_set --all) ] && clean="yes" && do_build="yes" && do_make_module="yes" && do_download="yes"
+[ $(is_opt_set --all) ] && do_clean="yes" && do_build="yes" && do_make_module="yes" && do_download="yes"
 
 version=$(get_opt_with --version)
-[ -z $version ] && version=1.64.0
-_version=$(echo $version | sed 's|\.|_|g')
-unpack_dir=$wdir/${module_name}_${_version}
-install_dir=${hepsoft_dir}/${module_name}/${version}
-remote_file=https://dl.bintray.com/boostorg/release/${version}/source/boost_${_version}.tar.gz
-local_file=`basename $remote_file`
+[ -z $version ] && version=3.9.0
+unpack_dir=${module_dir}/${module_name}-${version}
+install_dir=${module_dir}/${version}
+local_file=cmake-${version}.tar.gz
+remote_file=https://cmake.org/files/v3.9/${local_file}
 
 echo "[i] pdsf_modules: " $pdsf_modules
 echo "[i] module_name : " $module_name
 echo "[i] version     : " $version
 echo "[i] local_file  : " $local_file
+echo "[i] do_download : " $do_download
 
 module use ${hepsoft_dir}/modules
 if [ $(host_pdsf) ]; then
 	module load ${pdsf_modules}
-	[ $? ] && exit 1
+	[ $? != 0 ] && exit 1
 else
 	# module load cmake/3.9.0
 	echo "[i] no extra modules loaded"
 fi
 module list
 
-[ ${do_download} ] && rm -rf ${local_file} && wget $remote_file -O $local_file
+if [ "$do_download"="yes" ]; then
+	echo $do_download
+	cd ${module_dir}
+	rm -rf ${local_file}
+	wget $remote_file --no-check-certificate -O $local_file
+	cd $wdir
+fi
 
-if [ $clean ]; then
+if [ "${do_clean}"="yes" ]; then
+	cd ${module_dir}
 	echo "[i] cleaning ${unpack_dir}..."
 	rm -rf ${unpack_dir}
 	echo "[i] done cleaning."
+	cd $wdir
 fi
 
-if [ ${do_build} ]; then
+if [ "${do_build}"="yes" ]; then
+	cd ${module_dir}
 	[ ! -e $local_file ] && echo "[e] file $local_file does not exist" && exit 1
 	echo "[i] unpacking..."
 	tar zxvf $local_file 2>&1 > /dev/null
 	[ ! -d ${unpack_dir} ] && echo "[e] dir ${unpack_dir} does not exist" && exit 1
 	cd ${unpack_dir}
-	time ./bootstrap.sh --prefix=${install_dir}
-	time ./b2 install
+	./configure --prefix=${install_dir} --parallel=4 --qt-gui
+	make -j
+	make install
+	cd $wdir
 fi
 
 echo "[i] install_dir: ${install_dir}"
