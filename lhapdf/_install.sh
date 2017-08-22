5d0a344a0dc9e83918253c1710feb94fcbc875e2
matplo <ploskon@gmail.com>
2017-08-21 21:36:43 -0700

    moved to bt.sh

--- lhapdf/_install.sh
+++ lhapdf/_install.sh
@@ -1,48 +1,41 @@
 #!/bin/bash
 
-savedir=$PWD
-hepsoft_dir=<hepsoft>
-source ${hepsoft_dir}/bin/tools.sh
-process_variables $BASH_SOURCE $@
-cd $wdir
-echo_common_settings
-process_modules
-prep_build
+BT_install_prefix=<hepsoft>
+BT_module_paths=${BT_BT_install_prefix}/modules
+BT_modules=cmake
+BT_module_dir=${BT_install_prefix}/modules/${BT_name}
+
+BT_name=lhapdf
+BT_version=5.9.1
+BT_remote_file=http://www.hepforge.org/archive/lhapdf/lhapdf-5.9.1.tar.gz
+BT_pythonlib=python2.7/site-packages
 
 function get_PDFsets()
 {
 	savedir=$PWD
-	if [ ! -d ${install_dir} ]; then
-		echo "[w] no install dir ${install_dir} - making one.."
-		mkdir -pv ${install_dir}
-	fi
-    cd ${module_dir}
-    local _local_file=${module_dir}/PDFsets.tar.gz
+    check_install_paths
+    check_download_paths
+    cd ${BT_download_dir}
+    local _local_file=${BT_download_dir}/PDFsets.tar.gz
     if [ -e ${_local_file} ]; then
         echo "[i] ${_local_file} exists - will not download"
     else
         wget --no-check-certificate https://dl.dropboxusercontent.com/u/14190654/PDFsets/PDFsets.tar.gz
     fi
-    pdfsetdir=${install_dir}/share/lhapdf
+    pdfsetdir=${BT_install_dir}/share/lhapdf
     mkdir -p $pdfsetdir
     cd $pdfsetdir
     tar zxvf ${_local_file}
     ls -ltr $pdfsetdir/PDFsets
     echo "[i] installing PDFsets done."
     cd $savedir
-    echo $PWD
 }
 
 function build()
 {
-    ./configure --prefix=${install_dir}
-	[ ${do_clean} ] && make clean
+    cd ${BT_src_dir}
+    ./configure --prefix=${BT_install_dir}
 	make -j $(n_cores)
 	make install
     get_PDFsets
 }
-
-exec_build
-make_module
-
-cd $savedir
