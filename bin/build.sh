b654d7627e6f98bf0a37add6ef3fb3e3fb72fc53
matplo <ploskon@gmail.com>
2017-08-22 12:54:22 -0700

    download by default

--- bin/build.sh
+++ bin/build.sh
@@ -17,8 +17,8 @@ savedir=$PWD
 #rm -rf build_hepsoft
 mkdir -p ./build_hepsoft
 cd ./build_hepsoft
-# [ ! -f ./bt.sh ] && wget https://raw.github.com/matplo/buildtools/master/bt.sh && chmod +x ./bt.sh
-cp -v ~/devel/buildtools/bt.sh .
+[ ! -f ./bt.sh ] && wget https://raw.github.com/matplo/buildtools/master/bt.sh && chmod +x ./bt.sh
+# cp -v ~/devel/buildtools/bt.sh .
 source ./bt.sh
 
 cd $savedir
